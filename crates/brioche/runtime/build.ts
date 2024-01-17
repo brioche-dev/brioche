21c47f9096c2d92ee219c86bd20148939f86c46b
Kyle Lacy <kylelacy@kyle.space>
2024-01-17 01:51:21 -0800

    Switch to `esbuild-plugin-alias-path`
    The repo for `esbuild-plugin-alias` no longer exists, so it seems like
    it's no longer supported. `esbuild-plugin-alias-path` also seems to work
    and produced no actual build changes!

--- crates/brioche/runtime/build.ts
+++ crates/brioche/runtime/build.ts
@@ -1,5 +1,5 @@
 import * as esbuild from "esbuild";
-import alias from "esbuild-plugin-alias";
+import { aliasPath } from "esbuild-plugin-alias-path";
 import * as path from "node:path";
 
 await esbuild.build({
@@ -14,18 +14,20 @@ await esbuild.build({
     "__filename": "\"\"",
   },
   plugins: [
-    alias({
-      assert: path.resolve("./src/compat/node/assert.cjs"),
-      buffer: path.resolve("./src/compat/node/buffer.cjs"),
-      crypto: path.resolve("./src/compat/node/crypto.cjs"),
-      events: path.resolve("./src/compat/node/events.cjs"),
-      fs: path.resolve("./src/compat/node/fs.cjs"),
-      module: path.resolve("./src/compat/node/module.cjs"),
-      os: path.resolve("./src/compat/node/os.cjs"),
-      path: path.resolve("./src/compat/node/path.cjs"),
-      stream: path.resolve("./src/compat/node/stream.cjs"),
-      url: path.resolve("./src/compat/node/url.cjs"),
-      util: path.resolve("./src/compat/node/util.cjs"),
+    aliasPath({
+      alias: {
+        assert: path.resolve("./src/compat/node/assert.cjs"),
+        buffer: path.resolve("./src/compat/node/buffer.cjs"),
+        crypto: path.resolve("./src/compat/node/crypto.cjs"),
+        events: path.resolve("./src/compat/node/events.cjs"),
+        fs: path.resolve("./src/compat/node/fs.cjs"),
+        module: path.resolve("./src/compat/node/module.cjs"),
+        os: path.resolve("./src/compat/node/os.cjs"),
+        path: path.resolve("./src/compat/node/path.cjs"),
+        stream: path.resolve("./src/compat/node/stream.cjs"),
+        url: path.resolve("./src/compat/node/url.cjs"),
+        util: path.resolve("./src/compat/node/util.cjs"),
+      },
     }),
   ],
 });
