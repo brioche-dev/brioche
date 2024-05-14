d7715134ad2ee37071adc6f8f1e4fa6381716fd3
Kyle Lacy <kylewlacy@users.noreply.github.com>
2024-05-14 02:04:43 -0700

    Ensure Brioche can minimally run on platforms other than x86-64 Linux (#27)
    * Fix `create_input` when resource dir is not canonicalized
    
    This caused test failures when creating inputs on macOS (since
    `/tmp` is a symlink)
    
    * Fix more tests failing due to symlinked `/tmp` directory
    
    * Use `#[cfg]` attributes to segment of Linux-specific code
    
    Other platforms should now work, except when trying to bake `process`
    recipes (but the code is now segmented to make adding support for
    other platforms easier)
    
    * Tweak CI workflow to use a matrix (currently one entry)
    
    * Add macOS aarch64/x86-64 to CI
    
    * Update CI to run tests on macOS
    
    * Fix CI build failing when `tree` command is not found
    
    * Fix CI build trying to download non-existent artifact

--- crates/brioche-packed-userland-exec/src/main.rs
+++ crates/brioche-packed-userland-exec/src/main.rs
@@ -1,177 +1,18 @@
-#![cfg_attr(not(test), no_main)]
+#![cfg_attr(all(target_os = "linux", not(test)), no_main)]
 
-use std::ffi::{CStr, CString};
+mod linux;
 
-use bstr::ByteSlice as _;
-
-const BRIOCHE_PACKED_ERROR: u8 = 121;
-
-extern "C" {
-    static environ: *const *const libc::c_char;
-}
-
-#[cfg_attr(not(test), no_mangle)]
-#[allow(clippy::missing_safety_doc)]
-pub unsafe extern "C" fn main(argc: libc::c_int, argv: *const *const libc::c_char) -> libc::c_int {
-    let mut args = vec![];
-    let mut env_vars = vec![];
-
-    let argc: usize = argc.try_into().unwrap_or(0);
-    for n in 0..argc {
-        let arg = unsafe { *argv.add(n) };
-
-        if arg.is_null() {
-            break;
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        #[cfg_attr(not(test), no_mangle)]
+        #[allow(clippy::missing_safety_doc)]
+        pub unsafe extern "C" fn main(argc: libc::c_int, argv: *const *const libc::c_char) -> libc::c_int {
+            linux::entrypoint(argc, argv)
         }
-
-        let arg = unsafe { CStr::from_ptr(arg) };
-        args.push(arg);
-    }
-
-    for n in 0.. {
-        let var = unsafe { *environ.add(n) };
-
-        if var.is_null() {
-            break;
+    } else {
+        fn main() {
+            eprintln!("brioche-packed-userland-exec is only supported on Linux");
+            std::process::exit(1);
         }
-
-        let var = unsafe { CStr::from_ptr(var) };
-        env_vars.push(var);
-    }
-
-    let result = run(&args, &env_vars);
-    match result {
-        Ok(()) => libc::EXIT_SUCCESS,
-        Err(err) => {
-            eprintln!("brioche-packed error: {err}");
-            BRIOCHE_PACKED_ERROR.into()
-        }
-    }
-}
-
-fn run(args: &[&CStr], env_vars: &[&CStr]) -> Result<(), PackedError> {
-    let path = std::env::current_exe()?;
-    let resource_dir = brioche_pack::find_resource_dir(&path)?;
-    let mut program = std::fs::File::open(&path)?;
-    let pack = brioche_pack::extract_pack(&mut program)?;
-
-    match pack.interpreter {
-        Some(brioche_pack::Interpreter::LdLinux {
-            path: interpreter,
-            library_paths,
-        }) => {
-            let interpreter = interpreter
-                .to_path()
-                .map_err(|_| PackedError::InvalidPath)?;
-            let interpreter = resource_dir.join(interpreter);
-
-            let program = pack
-                .program
-                .to_path()
-                .map_err(|_| PackedError::InvalidPath)?;
-            let program = resource_dir.join(program).canonicalize()?;
-            let mut exec = userland_execve::ExecOptions::new(&interpreter);
-
-            let interpreter =
-                <[u8]>::from_path(&interpreter).ok_or_else(|| PackedError::InvalidPath)?;
-            let interpreter = CString::new(interpreter).map_err(|_| PackedError::InvalidPath)?;
-
-            // Add argv0
-            exec.arg(interpreter);
-
-            if !library_paths.is_empty() {
-                let mut ld_library_path = bstr::BString::default();
-                for (n, library_path) in library_paths.iter().enumerate() {
-                    let library_path = library_path
-                        .to_path()
-                        .map_err(|_| PackedError::InvalidPath)?;
-                    let library_path = resource_dir.join(library_path);
-
-                    if n > 0 {
-                        ld_library_path.push(b':');
-                    }
-
-                    let path =
-                        <[u8]>::from_path(&library_path).ok_or_else(|| PackedError::InvalidPath)?;
-                    ld_library_path.extend(path);
-                }
-
-                if let Some(env_library_path) = std::env::var_os("LD_LIBRARY_PATH") {
-                    let env_library_path = <[u8]>::from_os_str(&env_library_path)
-                        .ok_or_else(|| PackedError::InvalidPath)?;
-                    if !env_library_path.is_empty() {
-                        ld_library_path.push(b':');
-                        ld_library_path.extend(env_library_path);
-                    }
-                }
-
-                exec.arg(CStr::from_bytes_with_nul(b"--library-path\0").unwrap());
-
-                let ld_library_path =
-                    CString::new(ld_library_path).map_err(|_| PackedError::InvalidPath)?;
-                exec.arg(ld_library_path);
-            }
-
-            let mut args = args.iter();
-            if let Some(arg0) = args.next() {
-                exec.arg(CStr::from_bytes_with_nul(b"--argv0\0").unwrap());
-                exec.arg(arg0);
-            }
-
-            let program = <[u8]>::from_path(&program).ok_or_else(|| PackedError::InvalidPath)?;
-            let program = CString::new(program).map_err(|_| PackedError::InvalidPath)?;
-            exec.arg(program);
-
-            exec.args(args);
-
-            exec.env_pairs(env_vars);
-
-            userland_execve::exec_with_options(exec);
-        }
-        None => {
-            unimplemented!("execution without an interpreter");
-        }
-    }
-}
-
-#[derive(Debug, thiserror::Error)]
-enum PackedError {
-    IoError(#[from] std::io::Error),
-    ExtractPackError(#[from] brioche_pack::ExtractPackError),
-    PackResourceDirError(#[from] brioche_pack::PackResourceDirError),
-    InvalidPath,
-}
-
-impl std::fmt::Display for PackedError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(error_summary(self))
-    }
-}
-
-fn error_summary(error: &PackedError) -> &'static str {
-    match error {
-        PackedError::IoError(_) => "io error",
-        PackedError::ExtractPackError(error) => match error {
-            brioche_pack::ExtractPackError::ReadPackedProgramError(_) => {
-                "failed to read packed program: io error"
-            }
-            brioche_pack::ExtractPackError::MarkerNotFound => {
-                "marker not found at the end of the packed program"
-            }
-            brioche_pack::ExtractPackError::MalformedMarker => {
-                "malformed marker at the end of the packed program"
-            }
-            brioche_pack::ExtractPackError::InvalidPack(_) => "failed to parse pack: bincode error",
-        },
-        PackedError::PackResourceDirError(error) => match error {
-            brioche_pack::PackResourceDirError::NotFound => "brioche pack resource dir not found",
-            brioche_pack::PackResourceDirError::DepthLimitReached => {
-                "reached depth limit while searching for brioche pack resource dir"
-            }
-            brioche_pack::PackResourceDirError::IoError(_) => {
-                "error while searching for brioche pack resource dir: io error"
-            }
-        },
-        PackedError::InvalidPath => "invalid path",
     }
 }
---
+++ crates/brioche/src/sandbox/linux.rs
@@ -0,0 +1,186 @@
+#![cfg(target_os = "linux")]
+
+use std::{collections::HashMap, ffi::OsString, path::PathBuf};
+
+use bstr::ByteSlice as _;
+
+use super::{
+    ExitStatus, HostPathMode, SandboxPath, SandboxPathOptions, SandboxTemplate,
+    SandboxTemplateComponent,
+};
+
+pub fn run_sandbox(exec: super::SandboxExecutionConfig) -> anyhow::Result<super::ExitStatus> {
+    let mut host_paths = exec.include_host_paths;
+
+    let sandbox_host_dir = exec.sandbox_root.join("mnt").join("brioche-host");
+    std::fs::create_dir_all(&sandbox_host_dir)?;
+
+    let program = build_template(&exec.command, &mut host_paths)?;
+    let args = exec
+        .args
+        .iter()
+        .map(|arg| build_template(arg, &mut host_paths))
+        .collect::<anyhow::Result<Vec<_>>>()?;
+    let env = exec
+        .env
+        .iter()
+        .map(|(key, value)| {
+            let key = key.to_os_str()?.to_owned();
+            let value = build_template(value, &mut host_paths)?;
+            anyhow::Ok((key, value))
+        })
+        .collect::<anyhow::Result<HashMap<_, _>>>()?;
+
+    let mut command = unshare::Command::new(program);
+    command.args(&args);
+    command.env_clear();
+    command.envs(env);
+
+    let current_dir = build_template(
+        &SandboxTemplate {
+            components: vec![SandboxTemplateComponent::Path(exec.current_dir.clone())],
+        },
+        &mut host_paths,
+    )?;
+    command.current_dir(current_dir);
+
+    let host_uid = nix::unistd::Uid::current().as_raw();
+    let host_gid = nix::unistd::Gid::current().as_raw();
+    command.set_id_maps(
+        vec![unshare::UidMap {
+            inside_uid: exec.uid_hint,
+            outside_uid: host_uid,
+            count: 1,
+        }],
+        vec![unshare::GidMap {
+            inside_gid: exec.gid_hint,
+            outside_gid: host_gid,
+            count: 1,
+        }],
+    );
+    command.uid(exec.uid_hint);
+    command.gid(exec.gid_hint);
+    command.deny_setgroups(true);
+
+    command.unshare([
+        &unshare::Namespace::Mount,
+        &unshare::Namespace::Net,
+        &unshare::Namespace::User,
+    ]);
+
+    command.pivot_root(&exec.sandbox_root, &sandbox_host_dir, true);
+    command.before_chroot({
+        let sandbox_root = exec.sandbox_root.clone();
+        move || {
+            for (path, options) in &host_paths {
+                let path_metadata = path.metadata().map_err(|error| {
+                    std::io::Error::new(
+                        error.kind(),
+                        format!(
+                            "error getting metadata for path {}: {error}",
+                            path.display()
+                        ),
+                    )
+                })?;
+
+                let guest_path = options.guest_path_hint.to_path().map_err(|_| {
+                    std::io::Error::new(std::io::ErrorKind::Other, "invalid guest path")
+                })?;
+                let guest_path_under_root = guest_path.strip_prefix("/").map_err(|error| {
+                    std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        format!("invalid guest path: {error}"),
+                    )
+                })?;
+                let dest_path = sandbox_root.join(guest_path_under_root);
+
+                // Create either an empty file or directory as the mountpoint
+                if path_metadata.is_dir() {
+                    std::fs::create_dir_all(&dest_path)?;
+                } else {
+                    if let Some(dest_parent) = dest_path.parent() {
+                        std::fs::create_dir_all(dest_parent)?;
+                    }
+
+                    std::fs::write(&dest_path, "")?;
+                }
+
+                let readonly = match options.mode {
+                    HostPathMode::Read => true,
+                    HostPathMode::ReadWriteCreate => false,
+                };
+
+                libmount::BindMount::new(path, &dest_path)
+                    .readonly(readonly)
+                    .recursive(true)
+                    .mount()
+                    .map_err(|error| {
+                        std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            format!(
+                                "failed to mount {} -> {}: {error}",
+                                path.display(),
+                                dest_path.display()
+                            ),
+                        )
+                    })?;
+            }
+
+            libmount::BindMount::new(&sandbox_root, &sandbox_root)
+                .recursive(true)
+                .readonly(true)
+                .mount()
+                .map_err(|error| {
+                    std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        format!("failed to remount rootfs: {error}"),
+                    )
+                })?;
+
+            Ok(())
+        }
+    });
+
+    let mut child = command
+        .spawn()
+        .map_err(|error| anyhow::anyhow!("failed to spawn sandbox: {error}"))?;
+
+    let exit_status = child.wait()?;
+
+    let exit_status = match exit_status {
+        unshare::ExitStatus::Exited(code) => ExitStatus::Code(code),
+        unshare::ExitStatus::Signaled(signal, _) => ExitStatus::Signal(signal as i32),
+    };
+
+    Ok(exit_status)
+}
+
+fn build_template(
+    template: &SandboxTemplate,
+    host_paths: &mut HashMap<PathBuf, SandboxPathOptions>,
+) -> anyhow::Result<OsString> {
+    let mut result = bstr::BString::default();
+    for component in &template.components {
+        match component {
+            SandboxTemplateComponent::Literal { value } => {
+                result.extend_from_slice(value);
+            }
+            SandboxTemplateComponent::Path(SandboxPath { host_path, options }) => {
+                let existing_options = host_paths.insert(host_path.clone(), options.clone());
+                if let Some(existing_options) = existing_options {
+                    anyhow::ensure!(
+                        existing_options == *options,
+                        "tried to mount host path {} with conflicting mount options",
+                        host_path.display()
+                    );
+                }
+
+                let guest_path = &options.guest_path_hint;
+                result.extend_from_slice(guest_path);
+            }
+        }
+    }
+
+    let result = result.to_os_str()?;
+    Ok(result.to_owned())
+}
