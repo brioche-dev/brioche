e8c3457701c105d6f96cd18ae06e86574da1daec
jaudiger <jeremy.audiger@ioterop.com>
2024-07-06 08:49:32 +0200

    fix: struct `AsPath` is never constructed
    Signed-off-by: jaudiger <jeremy.audiger@ioterop.com>

--- crates/brioche-pack/src/encoding.rs
+++ crates/brioche-pack/src/encoding.rs
@@ -1,6 +1,4 @@
-use std::{borrow::Cow, path::PathBuf};
-
-use bstr::{ByteSlice, ByteVec as _};
+use std::borrow::Cow;
 
 pub enum TickEncoded {}
 
@@ -31,37 +29,5 @@ where
             tick_encoding::decode(encoded.as_bytes()).map_err(serde::de::Error::custom)?;
         let deserialized = T::try_from(decoded.into_owned()).map_err(serde::de::Error::custom)?;
         Ok(deserialized)
-    }
-}
-
-pub struct AsPath<T>(std::marker::PhantomData<T>);
-
-impl<T> serde_with::SerializeAs<PathBuf> for AsPath<T>
-where
-    T: serde_with::SerializeAs<Vec<u8>>,
-{
-    fn serialize_as<S>(source: &PathBuf, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let bytes = Vec::<u8>::from_path_buf(source.clone())
-            .map_err(|_| serde::ser::Error::custom("invalid path"))?;
-        T::serialize_as(&bytes, serializer)
-    }
-}
-
-impl<'de, T> serde_with::DeserializeAs<'de, PathBuf> for AsPath<T>
-where
-    T: serde_with::DeserializeAs<'de, Vec<u8>>,
-{
-    fn deserialize_as<D>(deserializer: D) -> Result<PathBuf, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let bytes: Vec<u8> = T::deserialize_as(deserializer)?;
-        let path = bytes
-            .to_path()
-            .map_err(|_| serde::de::Error::custom("invalid path"))?;
-        Ok(path.to_owned())
     }
 }
