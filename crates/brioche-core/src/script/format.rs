e80c0e1fc3130e3474bd47e985233f41d9b681fa
Jérémy Audiger <31616285+jaudiger@users.noreply.github.com>
2024-07-14 06:38:09 +0200

    feat: rewrite the format function to add the formatted files and to better handle errors happened on a project when formatting more than once at the same time (#86)
    Signed-off-by: jaudiger <jeremy.audiger@ioterop.com>

--- crates/brioche-core/src/script/format.rs
+++ crates/brioche-core/src/script/format.rs
@@ -2,41 +2,61 @@ use std::path::PathBuf;
 
 use crate::project::{ProjectHash, Projects};
 
-#[tracing::instrument(skip(projects), err)]
-pub async fn format(projects: &Projects, project_hash: ProjectHash) -> anyhow::Result<()> {
-    let module_paths = projects.project_module_paths(project_hash)?;
-    for path in &module_paths {
-        let contents = tokio::fs::read_to_string(path).await?;
-
-        let formatted_contents = format_code(&contents)?;
-
-        tokio::fs::write(path, &formatted_contents).await?;
-    }
-
-    Ok(())
+/// Formats the specified project using the provided formatter.
+///
+/// This function takes a reference to the `Projects` struct, a `ProjectHash` representing the project to format.
+/// It returns a `Result` containing a vector of `PathBuf` representing the paths of the formatted files,
+/// or an `anyhow::Error` if an error occurs.
+pub async fn format(
+    projects: &Projects,
+    project_hash: ProjectHash,
+) -> anyhow::Result<Vec<PathBuf>> {
+    format_project(projects, project_hash, false).await
 }
 
-#[tracing::instrument(skip(projects), err)]
+/// Checks the formatting of the specified project using the provided formatter.
+///
+/// This function takes a reference to the `Projects` struct, a `ProjectHash` representing the project to check.
+/// It returns a `Result` containing a vector of `PathBuf` representing the paths of the unformatted files,
+/// or an `anyhow::Error` if an error occurs.
 pub async fn check_format(
     projects: &Projects,
     project_hash: ProjectHash,
 ) -> anyhow::Result<Vec<PathBuf>> {
-    let mut unformatted = vec![];
+    format_project(projects, project_hash, true).await
+}
+
+#[tracing::instrument(skip(projects), err)]
+async fn format_project(
+    projects: &Projects,
+    project_hash: ProjectHash,
+    check: bool,
+) -> anyhow::Result<Vec<PathBuf>> {
+    let mut result = vec![];
 
     let module_paths = projects.project_module_paths(project_hash)?;
-    for path in &module_paths {
-        let contents = tokio::fs::read_to_string(path).await?;
+    for path in module_paths {
+        let contents = tokio::fs::read_to_string(&path).await?;
 
         let formatted_contents = format_code(&contents)?;
 
+        // Check if the file was formatted
         if contents != formatted_contents {
-            unformatted.push(path.to_owned());
+            if !check {
+                tokio::fs::write(&path, &formatted_contents).await?;
+            }
+
+            result.push(path);
         }
     }
 
-    Ok(unformatted)
+    Ok(result)
 }
 
+/// Formats the code using the specified formatter.
+///
+/// This function takes a string slice `contents` as input and formats the code using the `biome_js_formatter` crate.
+/// It returns a `Result` containing the formatted code as a `String`, or an `anyhow::Error` if an error occurs.
 pub fn format_code(contents: &str) -> anyhow::Result<String> {
     let file_source =
         biome_js_syntax::JsFileSource::ts().with_module_kind(biome_js_syntax::ModuleKind::Module);
