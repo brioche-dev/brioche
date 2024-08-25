9babadccb786a0b571eeb994b36e9f11f66720a3
Kyle Lacy <kylelacy@kyle.space>
2024-08-24 19:54:41 -0700

    Refactor `create_input` to use a graph for building artifacts

--- crates/brioche-core/src/input.rs
+++ crates/brioche-core/src/input.rs
@@ -6,8 +6,9 @@ use std::{
 
 use anyhow::Context as _;
 use bstr::{ByteSlice as _, ByteVec as _};
+use petgraph::visit::EdgeRef as _;
 
-use crate::fs_utils::{is_executable, set_directory_rwx_recursive};
+use crate::fs_utils::{is_executable, logical_path_bytes, set_directory_rwx_recursive};
 
 use super::{
     recipe::{Artifact, Directory, File, Meta, WithMeta},
@@ -46,9 +47,127 @@ pub async fn create_input(
     Ok(result)
 }
 
-#[async_recursion::async_recursion]
 #[tracing::instrument(skip_all, err, fields(input_path = %options.input_path.display()))]
 pub async fn create_input_inner(
+    brioche: &Brioche,
+    options: InputOptions<'_>,
+    buffer: &mut Vec<u8>,
+) -> anyhow::Result<WithMeta<Artifact>> {
+    let (plan, root_node) = tokio::task::spawn_blocking({
+        let input_path = options.input_path.to_owned();
+        let remove_input = options.remove_input;
+        let resource_dir = options.resource_dir.map(|path| path.to_owned());
+        let input_resource_dirs = options.input_resource_dirs.to_owned();
+        let meta = options.meta.clone();
+        move || {
+            let mut plan = CreateInputPlan::default();
+            let root_node = plan_create_input_sync(
+                InputOptions {
+                    input_path: &input_path,
+                    remove_input,
+                    resource_dir: resource_dir.as_deref(),
+                    input_resource_dirs: &input_resource_dirs,
+                    saved_paths: &mut Default::default(),
+                    meta: &meta,
+                },
+                &mut plan,
+            )?;
+
+            anyhow::Ok((plan, root_node))
+        }
+    })
+    .await??;
+
+    let mut nodes_to_artifacts = HashMap::<petgraph::graph::NodeIndex, WithMeta<Artifact>>::new();
+    let planned_nodes = petgraph::algo::toposort(petgraph::visit::Reversed(&plan.graph), None)
+        .map_err(|_| anyhow::anyhow!("cycle detected in input {:?}", options.input_path))?;
+
+    for node_index in planned_nodes {
+        let node = &plan.graph[node_index];
+        let path = plan.nodes_to_paths[&node_index].to_owned();
+
+        let artifact = match node {
+            CreateInputPlanNode::File { is_executable } => {
+                let content_blob = super::blob::save_blob_from_file(
+                    brioche,
+                    &mut super::blob::get_save_blob_permit().await?,
+                    &path,
+                    super::blob::SaveBlobOptions::default().remove_input(options.remove_input),
+                    buffer,
+                )
+                .await?;
+
+                let mut resources = Directory::default();
+
+                let edges_out = plan
+                    .graph
+                    .edges_directed(node_index, petgraph::Direction::Outgoing);
+                for edge in edges_out {
+                    let resource_path = match &edge.weight() {
+                        CreateInputPlanEdge::Resource { path } => path,
+                        CreateInputPlanEdge::IndirectResource { path } => path,
+                        _ => anyhow::bail!("unexpected edge for file node: {:?}", edge.weight()),
+                    };
+
+                    let resource_node_index = edge.target();
+                    let resource_artifact = nodes_to_artifacts
+                        .get(&resource_node_index)
+                        .ok_or_else(|| anyhow::anyhow!("resource node not found"))?;
+
+                    resources
+                        .insert(brioche, resource_path, Some(resource_artifact.clone()))
+                        .await?;
+                }
+
+                Artifact::File(File {
+                    content_blob,
+                    executable: *is_executable,
+                    resources,
+                })
+            }
+            CreateInputPlanNode::Symlink { target } => Artifact::Symlink {
+                target: target.clone(),
+            },
+            CreateInputPlanNode::Directory => {
+                let mut directory = Directory::default();
+
+                let edges_out = plan
+                    .graph
+                    .edges_directed(node_index, petgraph::Direction::Outgoing);
+                for edge in edges_out {
+                    let entry_name = match &edge.weight() {
+                        CreateInputPlanEdge::DirectoryEntry { file_name } => file_name,
+                        _ => {
+                            anyhow::bail!("unexpected edge for directory node: {:?}", edge.weight())
+                        }
+                    };
+
+                    let entry_node_index = edge.target();
+                    let entry_artifact = nodes_to_artifacts
+                        .get(&entry_node_index)
+                        .ok_or_else(|| anyhow::anyhow!("directory entry node not found"))?;
+
+                    directory
+                        .insert(brioche, entry_name, Some(entry_artifact.clone()))
+                        .await?;
+                }
+
+                Artifact::Directory(directory)
+            }
+        };
+
+        nodes_to_artifacts.insert(node_index, WithMeta::new(artifact, options.meta.clone()));
+    }
+
+    let artifact = nodes_to_artifacts
+        .remove(&root_node)
+        .ok_or_else(|| anyhow::anyhow!("root node not found"))?;
+    Ok(artifact)
+}
+
+#[async_recursion::async_recursion]
+#[tracing::instrument(skip_all, err, fields(input_path = %options.input_path.display()))]
+pub async fn create_input_inner_old(
     brioche: &Brioche,
     options: InputOptions<'async_recursion>,
     buffer: &mut Vec<u8>,
@@ -337,4 +456,273 @@ async fn find_resource<'a>(
     }
 
     Ok(None)
+}
+
+fn find_resource_sync<'a>(
+    resource_dir: Option<&'a Path>,
+    input_resource_dirs: &'a [PathBuf],
+    subpath: &Path,
+) -> anyhow::Result<Option<FoundResource<'a>>> {
+    let resource_dirs = resource_dir
+        .into_iter()
+        .chain(input_resource_dirs.iter().map(|dir| &**dir));
+
+    for resource_dir in resource_dirs {
+        let resource_path = resource_dir.join(subpath);
+        let resource_metadata = std::fs::symlink_metadata(&resource_path);
+        match resource_metadata {
+            Ok(metadata) => {
+                return Ok(Some(FoundResource {
+                    path: resource_path,
+                    metadata,
+                    resource_dir,
+                }));
+            }
+            Err(err) if err.kind() == std::io::ErrorKind::NotFound => {}
+            Err(err) => {
+                return Err(err).context("failed to get metadata for resource");
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+#[derive(Debug, Clone)]
+enum CreateInputPlanNode {
+    File { is_executable: bool },
+    Directory,
+    Symlink { target: bstr::BString },
+}
+
+#[derive(Debug, Clone)]
+enum CreateInputPlanEdge {
+    DirectoryEntry { file_name: bstr::BString },
+    Resource { path: bstr::BString },
+    IndirectResource { path: bstr::BString },
+    SymlinkTarget,
+}
+
+#[derive(Default)]
+struct CreateInputPlan {
+    graph: petgraph::graph::DiGraph<CreateInputPlanNode, CreateInputPlanEdge>,
+    paths_to_nodes: HashMap<PathBuf, petgraph::graph::NodeIndex>,
+    nodes_to_paths: HashMap<petgraph::graph::NodeIndex, PathBuf>,
+}
+
+fn plan_create_input_sync(
+    options: InputOptions,
+    plan: &mut CreateInputPlan,
+) -> anyhow::Result<petgraph::graph::NodeIndex> {
+    if let Some(node) = plan.paths_to_nodes.get(options.input_path) {
+        return Ok(*node);
+    }
+
+    let metadata =
+        std::fs::symlink_metadata(options.input_path).context("failed to get metadata")?;
+
+    let root_node = if metadata.is_file() {
+        let is_executable = is_executable(&metadata.permissions());
+        let node_index = plan
+            .graph
+            .add_node(CreateInputPlanNode::File { is_executable });
+
+        plan.paths_to_nodes
+            .insert(options.input_path.to_owned(), node_index);
+        plan.nodes_to_paths
+            .insert(node_index, options.input_path.to_owned());
+
+        let mut file = std::fs::File::open(options.input_path)
+            .with_context(|| format!("failed to open file {:?}", options.input_path))?;
+        let extracted = brioche_pack::extract_pack(&mut file).ok();
+        let pack = extracted.map(|extracted| extracted.pack);
+        let resource_paths = pack.into_iter().flat_map(|pack| pack.paths());
+        for resource_path in resource_paths {
+            let resource_subpath = resource_path.to_path().context("invalid resource path")?;
+            let resource = find_resource_sync(
+                options.resource_dir,
+                options.input_resource_dirs,
+                resource_subpath,
+            )?
+            .with_context(|| format!("resource not found: {resource_path:?}"))?;
+
+            let resource_node = plan_create_input_sync(
+                InputOptions {
+                    input_path: &resource.path,
+                    remove_input: false,
+                    resource_dir: options.resource_dir,
+                    input_resource_dirs: options.input_resource_dirs,
+                    saved_paths: options.saved_paths,
+                    meta: options.meta,
+                },
+                plan,
+            )?;
+
+            plan.graph.update_edge(
+                node_index,
+                resource_node,
+                CreateInputPlanEdge::Resource {
+                    path: resource_path,
+                },
+            );
+        }
+
+        node_index
+    } else if metadata.is_symlink() {
+        // TODO: Handle broken links
+
+        let target =
+            std::fs::read_link(options.input_path).context("failed to read symlink target")?;
+        let target = <Vec<u8>>::from_path_buf(target).map_err(|_| {
+            anyhow::anyhow!("invalid symlink target at {}", options.input_path.display())
+        })?;
+        let target = bstr::BString::from(target);
+
+        let node_index = plan.graph.add_node(CreateInputPlanNode::Symlink { target });
+
+        plan.paths_to_nodes
+            .insert(options.input_path.to_owned(), node_index);
+        plan.nodes_to_paths
+            .insert(node_index, options.input_path.to_owned());
+
+        let canonical_path = std::fs::canonicalize(options.input_path)
+            .context("failed to canonicalize symlink path")?;
+        let resource_dirs = options
+            .resource_dir
+            .into_iter()
+            .chain(options.input_resource_dirs.iter().map(|dir| &**dir));
+        for resource_dir in resource_dirs {
+            let canonical_resource_dir = std::fs::canonicalize(resource_dir)
+                .context("failed to canonicalize resource dir")?;
+            if canonical_path.starts_with(&canonical_resource_dir) {
+                let target_node = plan_create_input_sync(
+                    InputOptions {
+                        input_path: &canonical_path,
+                        remove_input: false,
+                        resource_dir: options.resource_dir,
+                        input_resource_dirs: options.input_resource_dirs,
+                        saved_paths: options.saved_paths,
+                        meta: options.meta,
+                    },
+                    plan,
+                )?;
+
+                plan.graph
+                    .update_edge(node_index, target_node, CreateInputPlanEdge::SymlinkTarget);
+            }
+        }
+
+        node_index
+    } else if metadata.is_dir() {
+        let node_index = plan.graph.add_node(CreateInputPlanNode::Directory);
+
+        plan.paths_to_nodes
+            .insert(options.input_path.to_owned(), node_index);
+        plan.nodes_to_paths
+            .insert(node_index, options.input_path.to_owned());
+
+        let dir = std::fs::read_dir(options.input_path)?;
+        for dir_entry in dir {
+            let dir_entry = dir_entry?;
+            let file_name = dir_entry.file_name();
+            let file_name = <Vec<u8>>::from_os_string(file_name).map_err(|_| {
+                anyhow::anyhow!(
+                    "invalid file name {:?} in directory {}",
+                    dir_entry.file_name(),
+                    options.input_path.display()
+                )
+            })?;
+            let file_name = bstr::BString::from(file_name);
+
+            let dir_entry_node = plan_create_input_sync(
+                InputOptions {
+                    input_path: &dir_entry.path(),
+                    remove_input: false,
+                    resource_dir: options.resource_dir,
+                    input_resource_dirs: options.input_resource_dirs,
+                    saved_paths: options.saved_paths,
+                    meta: options.meta,
+                },
+                plan,
+            )?;
+
+            plan.graph.update_edge(
+                node_index,
+                dir_entry_node,
+                CreateInputPlanEdge::DirectoryEntry { file_name },
+            );
+        }
+
+        node_index
+    } else {
+        anyhow::bail!("unsupported file type at {:?}", options.input_path);
+    };
+
+    let mut resource_paths: Vec<_> = plan
+        .graph
+        .edge_references()
+        .filter_map(|edge| {
+            let resource_path = match edge.weight() {
+                CreateInputPlanEdge::Resource { path } => path,
+                _ => return None,
+            };
+
+            let node = edge.source();
+            let resource_node = edge.target();
+
+            Some((node, resource_path.clone(), resource_node))
+        })
+        .collect();
+
+    let mut indirect_resources = vec![];
+
+    while let Some((node, resource_path, resource_node)) = resource_paths.pop() {
+        for subresource_edge in plan.graph.edges(resource_node) {
+            match subresource_edge.weight() {
+                CreateInputPlanEdge::DirectoryEntry { file_name } => {
+                    let mut subresource_path = resource_path.clone();
+                    subresource_path.extend_from_slice(b"/");
+                    subresource_path.extend_from_slice(file_name);
+
+                    let subresource_node = subresource_edge.target();
+
+                    resource_paths.push((node, subresource_path, subresource_node));
+                }
+                CreateInputPlanEdge::Resource { path }
+                | CreateInputPlanEdge::IndirectResource { path } => {
+                    let subresource_node = subresource_edge.target();
+
+                    resource_paths.push((node, path.clone(), subresource_node));
+                }
+                CreateInputPlanEdge::SymlinkTarget => {
+                    let target = match &plan.graph[resource_node] {
+                        CreateInputPlanNode::Symlink { target } => target,
+                        _ => anyhow::bail!("non-symlink node has symlink target edge"),
+                    };
+
+                    let mut target_path = resource_path.clone();
+                    target_path.extend_from_slice(b"/../");
+                    target_path.extend_from_slice(target);
+
+                    let target_path =
+                        logical_path_bytes(&target_path).context("invalid symlink target")?;
+                    let target_path = bstr::BString::from(target_path);
+
+                    let target_node = subresource_edge.target();
+
+                    indirect_resources.push((
+                        node,
+                        target_node,
+                        CreateInputPlanEdge::IndirectResource { path: target_path },
+                    ));
+                }
+            }
+        }
+    }
+
+    for (node, target_node, weight) in indirect_resources {
+        plan.graph.update_edge(node, target_node, weight);
+    }
+
+    Ok(root_node)
 }
