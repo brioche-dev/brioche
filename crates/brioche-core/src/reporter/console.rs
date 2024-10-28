65233610dd0183efcf6bcc8d996639dd5e9f3faa
Kyle Lacy <kylelacy@kyle.space>
2024-10-27 17:06:03 -0700

    Update `JobOutputContents.pop_contents()` to pop the oldest content

--- crates/brioche-core/src/reporter/console.rs
+++ crates/brioche-core/src/reporter/console.rs
@@ -1047,7 +1047,7 @@ impl JobOutputContents {
     }
 
     fn pop_contents(&mut self) -> Option<(JobOutputStream, bstr::BString)> {
-        let content = self.contents.pop_back();
+        let content = self.contents.pop_front();
         if let Some((_, content)) = &content {
             self.total_bytes = self.total_bytes.saturating_sub(content.len());
         }
