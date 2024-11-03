3b0dde48a9f8d4baaec4e085de850eac9d90afa9
Kyle Lacy <kylelacy@kyle.space>
2024-11-03 01:41:44 -0700

    Update `brioche jobs logs` to support compressed or decompressed events

--- crates/brioche/src/jobs/logs.rs
+++ crates/brioche/src/jobs/logs.rs
@@ -1,6 +1,16 @@
-use std::path::PathBuf;
+use std::{io::Seek as _, path::PathBuf};
 
-use brioche_core::process_events::display::{display_events, DisplayEventsOptions};
+use anyhow::Context as _;
+use brioche_core::{
+    process_events::{
+        display::{display_events, DisplayEventsOptions},
+        PROCESS_EVENT_MAGIC,
+    },
+    utils::{
+        io::NotSeekable,
+        zstd::{ZstdLinearDecoder, ZstdSmartDecoder},
+    },
+};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -17,11 +27,50 @@ pub struct LogsArgs {
     reverse: bool,
 }
 
+const ZSTD_FRAME_MAGIC: &[u8] = &[0x28, 0xB5, 0x2F, 0xFD];
+const ZSTD_SKIPPABLE_FRAME_MAGIC: &[u8] = &[0x2A, 0x4D, 0x18];
+
+trait ReadSeek: std::io::Read + std::io::Seek {}
+impl<T: std::io::Read + std::io::Seek> ReadSeek for T {}
+
 pub fn logs(args: LogsArgs) -> anyhow::Result<()> {
-    let input = brioche_core::utils::zstd::ZstdSmartDecoder::create(|| {
-        let input = std::fs::File::open(&args.path)?;
-        anyhow::Ok(std::io::BufReader::new(input))
-    })?;
+    let input: Box<dyn ReadSeek> = if args.path.to_str() == Some("-") {
+        let mut stdin = std::io::stdin().lock();
+        let format = detect_format(&mut stdin)?;
+
+        match format {
+            LogFileFormat::Zstd => {
+                let decoder = ZstdLinearDecoder::new(stdin)?;
+                Box::new(NotSeekable(decoder))
+            }
+            LogFileFormat::Bin => Box::new(NotSeekable(stdin)),
+        }
+    } else {
+        let mut file = std::fs::File::open(&args.path)?;
+        let can_seek = file.stream_position().is_ok();
+        let mut buf_reader = std::io::BufReader::new(file);
+        let format = detect_format(&mut buf_reader)?;
+
+        match format {
+            LogFileFormat::Zstd => {
+                if can_seek {
+                    let mut file = buf_reader.into_inner();
+                    file.seek(std::io::SeekFrom::Start(0))
+                        .context("failed to seek to start of file")?;
+
+                    let decoder = ZstdSmartDecoder::create(|| {
+                        let file = file.try_clone().context("failed to clone file")?;
+                        anyhow::Ok(std::io::BufReader::new(file))
+                    })?;
+                    Box::new(decoder)
+                } else {
+                    let decoder = ZstdLinearDecoder::new(buf_reader)?;
+                    Box::new(decoder)
+                }
+            }
+            LogFileFormat::Bin => Box::new(buf_reader),
+        }
+    };
 
     let mut reader = brioche_core::process_events::reader::ProcessEventReader::new(input)?;
 
@@ -34,4 +83,36 @@ pub fn logs(args: LogsArgs) -> anyhow::Result<()> {
     )?;
 
     Ok(())
+}
+
+fn detect_format(reader: &mut impl std::io::BufRead) -> anyhow::Result<LogFileFormat> {
+    let buf = reader.fill_buf()?;
+
+    let process_event_magic = PROCESS_EVENT_MAGIC.as_bytes();
+    let process_event_magic_partial_len = process_event_magic.len().min(buf.len());
+    let process_event_magic_partial = &process_event_magic[0..process_event_magic_partial_len];
+    if buf.starts_with(process_event_magic_partial) {
+        return Ok(LogFileFormat::Bin);
+    }
+
+    let zstd_frame_magic_partial_len = ZSTD_FRAME_MAGIC.len().min(buf.len());
+    let zstd_frame_magic_partial = &ZSTD_FRAME_MAGIC[0..zstd_frame_magic_partial_len];
+    if buf.starts_with(zstd_frame_magic_partial) {
+        return Ok(LogFileFormat::Zstd);
+    }
+
+    let zstd_skippable_frame_magic_partial_len = ZSTD_SKIPPABLE_FRAME_MAGIC.len().min(buf.len());
+    let zstd_skippable_frame_magic_partial =
+        &ZSTD_SKIPPABLE_FRAME_MAGIC[0..zstd_skippable_frame_magic_partial_len];
+    if buf.starts_with(zstd_skippable_frame_magic_partial) {
+        return Ok(LogFileFormat::Zstd);
+    }
+
+    anyhow::bail!("could not detect log file format");
+}
+
+#[derive(Debug, Clone, Copy)]
+enum LogFileFormat {
+    Zstd,
+    Bin,
 }
