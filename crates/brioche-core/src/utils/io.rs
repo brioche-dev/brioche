9ec9927e48309862ece46716821f1d6c8e0e0133
Kyle Lacy <kylelacy@kyle.space>
2024-11-02 19:54:37 -0700

    Add `TrySeek` trait and `NotSeekable` type

--- crates/brioche-core/src/utils/io.rs
+++ crates/brioche-core/src/utils/io.rs
@@ -46,3 +46,64 @@ where
         Ok(new_pos)
     }
 }
+
+#[diagnostic::on_unimplemented(
+    message = "The type `{Self}` does not implement `std::io::Seek`",
+    label = "`{Self}` is not seekable",
+    note = "Wrap the type with `NotSeekable` for use with non-seekable types"
+)]
+pub trait TrySeek {
+    fn try_seek(&mut self, pos: std::io::SeekFrom) -> Option<std::io::Result<u64>>;
+}
+
+impl<T> TrySeek for T
+where
+    T: std::io::Seek,
+{
+    fn try_seek(&mut self, pos: std::io::SeekFrom) -> Option<std::io::Result<u64>> {
+        Some(self.seek(pos))
+    }
+}
+
+pub struct NotSeekable<T>(pub T);
+
+impl<T> std::io::Read for NotSeekable<T>
+where
+    T: std::io::Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+        self.0.read(buf)
+    }
+}
+
+impl<T> std::io::BufRead for NotSeekable<T>
+where
+    T: std::io::BufRead,
+{
+    fn fill_buf(&mut self) -> std::io::Result<&[u8]> {
+        self.0.fill_buf()
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.0.consume(amt)
+    }
+}
+
+impl<T> std::io::Write for NotSeekable<T>
+where
+    T: std::io::Write,
+{
+    fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error> {
+        self.0.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.0.flush()
+    }
+}
+
+impl<T> TrySeek for NotSeekable<T> {
+    fn try_seek(&mut self, _pos: std::io::SeekFrom) -> Option<std::io::Result<u64>> {
+        None
+    }
+}
