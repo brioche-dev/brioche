32c784b8842f3e725d5d508660b216dde8c98ec4
Kyle Lacy <kylelacy@kyle.space>
2025-02-24 00:33:46 -0800

    Add test to validate LSP respects content in "did open" request

--- crates/brioche-core/tests/lsp_completions.rs
+++ crates/brioche-core/tests/lsp_completions.rs
@@ -406,3 +406,73 @@ async fn test_lsp_completions_from_workspace_import() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn test_lsp_completions_in_unsaved_edited_file() -> anyhow::Result<()> {
+    let (_brioche, context, lsp) = brioche_test_support::brioche_lsp_test().await;
+
+    let project_root = context
+        .write_file(
+            "myproject/project.bri",
+            indoc::indoc! {r#"
+                // File on disk is effectively empty
+            "#},
+        )
+        .await;
+
+    // The existing file exists but doesn't match the content provided by
+    // the LSP client. This happens e.g. when re-opening a VS Code window
+    // that has unsaved file changes. In this case, we should use the client's
+    // view of the file's contents instead of what's actually on disk
+    lsp.notify::<notification::DidOpenTextDocument>(lsp_types::DidOpenTextDocumentParams {
+        text_document: TextDocumentItem::new(
+            Url::from_file_path(&project_root).unwrap(),
+            "brioche".to_string(),
+            1,
+            indoc::indoc! {r#"
+                function exampleFunction(): string {
+                    return "hello world!";
+                }
+
+                exampleFunc // <-- completion here
+            "#}
+            .to_string(),
+        ),
+    })
+    .await;
+
+    let completion_response = lsp
+        .request::<request::Completion>(lsp_types::CompletionParams {
+            text_document_position: TextDocumentPositionParams::new(
+                TextDocumentIdentifier::new(Url::from_file_path(&project_root).unwrap()),
+                Position::new(4, 11),
+            ),
+            context: None,
+            partial_result_params: Default::default(),
+            work_done_progress_params: Default::default(),
+        })
+        .await
+        .expect("no completion response returned");
+    let completion_items = match completion_response {
+        lsp_types::CompletionResponse::Array(completion_items) => completion_items,
+        lsp_types::CompletionResponse::List(completion_list) => {
+            assert!(
+                !completion_list.is_incomplete,
+                "expected complete completion list"
+            );
+            completion_list.items
+        }
+    };
+    assert!(!completion_items.is_empty(), "no completions returned");
+
+    let top_completion_item = &completion_items[0];
+    assert_eq!(
+        top_completion_item,
+        &lsp_types::CompletionItem {
+            label: "exampleFunction".to_string(),
+            ..Default::default()
+        },
+    );
+
+    Ok(())
+}
