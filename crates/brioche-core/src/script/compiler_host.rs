e51e9ccc41f18fb8d95cbb1edf1f3367f562e060
Kyle Lacy <kylelacy@kyle.space>
2025-03-01 20:51:53 -0800

    Remove old commented-out code

--- crates/brioche-core/src/script/compiler_host.rs
+++ crates/brioche-core/src/script/compiler_host.rs
@@ -236,40 +236,6 @@ impl BriocheCompilerHost {
             });
         }
 
-        //     let mut documents = self
-        //     .documents
-        //     .write()
-        //     .map_err(|_| anyhow::anyhow!("failed to acquire lock on documents"))?;
-        // documents.entry(specifier.clone()).and_modify(|doc| {
-        //     doc.version += 1;
-        // });
-
-        // match &specifier {
-        //     BriocheModuleSpecifier::File { path } => {
-        //         let project = self
-        //             .projects
-        //             .find_containing_project(path)
-        //             .with_context(|| format!("no project found for path '{}'", path.display()))?;
-
-        //         if let Some(project) = project {
-        //             self.projects.clear(project).await?;
-        //         }
-
-        //         self.projects
-        //             .load_from_module_path(&self.brioche, path, false)
-        //             .await?;
-
-        //         let mut documents = self
-        //             .documents
-        //             .write()
-        //             .map_err(|_| anyhow::anyhow!("failed to acquire lock on documents"))?;
-        //         documents.entry(specifier.clone()).and_modify(|doc| {
-        //             doc.version += 1;
-        //         });
-        //     }
-        //     BriocheModuleSpecifier::Runtime { .. } => {}
-        // }
-
         Ok(())
     }
 }
