b7b4fc679634f8414ff23313b55b9ac699f5737b
Jérémy Audiger <jeremy.audiger@icloud.com>
2025-12-16 21:25:13 +0100

    chore: sync tslib from node_modules
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- crates/brioche-core/runtime/tslib/lib.es2023.collection.d.ts
+++ crates/brioche-core/runtime/tslib/lib.es2023.collection.d.ts
@@ -16,29 +16,6 @@ and limitations under the License.
 
 /// <reference no-default-lib="true"/>
 
-declare namespace Intl {
-    interface DateTimeFormatPartTypesRegistry {
-        day: any;
-        dayPeriod: any;
-        era: any;
-        hour: any;
-        literal: any;
-        minute: any;
-        month: any;
-        second: any;
-        timeZoneName: any;
-        weekday: any;
-        year: any;
-    }
-
-    type DateTimeFormatPartTypes = keyof DateTimeFormatPartTypesRegistry;
-
-    interface DateTimeFormatPart {
-        type: DateTimeFormatPartTypes;
-        value: string;
-    }
-
-    interface DateTimeFormat {
-        formatToParts(date?: Date | number): DateTimeFormatPart[];
-    }
+interface WeakKeyTypes {
+    symbol: symbol;
 }
